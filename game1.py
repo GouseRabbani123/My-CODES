ff66c6b374f6fdbd05b0d44b4fa2eb31b5c1fbd7
GouseRabbani123 <67941879+GouseRabbani123@users.noreply.github.com>
2022-02-08 21:46:28 +0530

    Delete game1.py

--- game1.py
+++
@@ -1,8 +0,0 @@
-import pygame,sys,os
-from pygame.locals import *
-red=[255,0,0]
-
-pygame.init()
-
-window =pygame.display.set_mode((1000,600))
-pygame.display.set_caption('Slither - eat - the snake game')
